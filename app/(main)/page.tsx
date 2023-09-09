d8e07aab55f15ea58bb3a51bb0e581f4084d8a18
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-09-10 03:01:44 +0400

    feat: organizational routes & button component added

---
+++ app/(main)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="bg-black text-white">Hello App</p>
+    </div>
+  );
+}
