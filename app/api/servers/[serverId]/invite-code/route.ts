704adca94b62d66e69bbb4159e77ed16b6c9aafb
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-09-21 19:10:29 +0400

    feat: generate new link api created

---
+++ app/api/servers/[serverId]/invite-code/route.ts
@@ -0,0 +1,36 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized.", { status: 401 });
+    }
+
+    if (!params.serverId) {
+      return new NextResponse("Server id is missing.", { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+      data: {
+        inviteCode: uuidv4(),
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (err) {
+    console.error("[SERVER_ID]", err);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
